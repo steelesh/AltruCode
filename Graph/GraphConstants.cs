08062bbf2449457d6481c7985bf4171a5db59462
Steele <shrevesa@mail.uc.edu>
2023-05-05 21:54:09 -0400

    Create class for constants for working with user information from the Graph API

---
+++ Graph/GraphConstants.cs
@@ -0,0 +1,10 @@
+namespace AltruCode.Graph;
+
+public static class GraphConstants
+{
+    public static readonly string[] Scopes =
+    {
+        "User.Read",
+        "User.ReadBasic.All"
+    };
+}
